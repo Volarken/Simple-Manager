de8cc5aa1bac0fba2371d1dc54558befbbf2ad1b
Root <24883953+Volarken@users.noreply.github.com>
2021-08-05 16:31:51 -0700

    slight mod

--- SimpleManager.sh
+++ SimpleManager.sh
@@ -9,17 +9,32 @@ if test -d $HOME/SimpleManager/
      sudo mkdir $HOME/SimpleManager/
      echo Folder Created
 	 wget ##add downloads for all files here pls
+	 wget https://raw.githubusercontent.com/Volarken/Simple-Manager/main/autoupdate.sh
+	 wget https://raw.githubusercontent.com/Volarken/Simple-Manager/main/log.sh
+	 wget https://raw.githubusercontent.com/Volarken/Simple-Manager/main/discord_log.sh
+	 wget https://raw.githubusercontent.com/Volarken/Simple-Manager/main/global.var
+	 
 	 source $HOME/SimpleManager/global.var
       fi
 
-
-
-
+func_autoUpdate(){
+if [ "$APIVERSION" = "$WEBVERSION" ]; then
+LogInput="Script up to date, last update check ran on "
+func_logEvent
+else
+LogInput="Script is outdated, running update protocols on "
+func_logEvent
+sudo mkdir -p /usr/bin/betterssh
+sudo -s curl https://raw.githubusercontent.com/Volarken/betterssh/main/version.txt -o /usr/bin/betterssh/version.txt > /dev/null
+sudo -s curl -L https://raw.githubusercontent.com/Volarken/betterssh/main/betterssh.sh -o "$0" > /dev/null
+clear
+sudo bash "$0"
+fi
 
 func_requiredRepos () { 
 if [ $(dpkg-query -W -f='${Status}' python3 2>/dev/null | grep -c "ok installed") -eq 0 ];
 then
-  log "Missing python3" "One or more required repositories are not installed, will aquire now at $TIME0"
+  bash log "Missing python3" "One or more required repositories are not installed, will aquire now at $TIME0"
   echo You are missing required files, we will aquire them now. This may take a while. 
   read -p 'Press enter to continue.'
   sudo apt-get install python3
@@ -28,7 +43,7 @@ then
 fi
 if [ $(dpkg-query -W -f='${Status}' screen 2>/dev/null | grep -c "ok installed") -eq 0 ];
 then
-  log "Missing screen" "One or more required repositories are not installed, will aquire now at $TIME0"
+  bash log "Missing screen" "One or more required repositories are not installed, will aquire now at $TIME0"
   echo You are missing required files, we will aquire them now. This may take a while. 
   read -p 'Press enter to continue.'
   sudo apt-get install python3
@@ -37,6 +52,8 @@ then
 fi
 clear
 }
+
+
 
 
 
--- autoupdate.sh
+++ autoupdate.sh
@@ -5,32 +5,9 @@ source $HOME/SimpleManager/global.var
 ##Child Script Variables##
 declare -i DAYS
 ##
-discord_log () {
-WEBHOOK_URL="https://discord.com/api/webhooks/872891986129063956/-2EHHb2RzPlDeoFHNq_d3TmzgL0g4_qxZct-kgcxjUoapsZqTohvlZUgMs1txGyynEVC"
-tail -n $1 <$FILE   > logfile.txt
-curl \
-  -F 'payload_json={"username": "Botty McBotFace", "content": "@everyone"}' \
-  -F "file1=@logfile.txt" \
-  $WEBHOOK_URL
-  sleep 1;
-  rm logfile.txt
-}
 
-log () {
-sudo /bin/cat <<-EOM >>$FILE
-    Log created at $TIME0
-	Script Version="$APIVERSION" || Last SSH Connection $SSH_CONNECTION
-    ####################################################################################
-	${TAB}SERVER: $serverip${TAB}DETECTED ACTIVITY:
-	${TAB} $1 
-	${TAB} $2 
-	${TAB} $3
-	${TAB}END
-EOM
-}
-
-log "AutoUpdate/Restart script is now online."
-discord_log "7"
+bash log "AutoUpdate/Restart script is now online."
+bash bash discord_log "7"
 while [[ "0" = "0" ]]; do     #This is my lazy way of making sure that the script is constantly looping.
 TIME1=$(date +%H:%M)          #This stores the current time inside of a variable called TIME1
 sleep 10;                     #This puts a delay on how fast the script can run, this stops the script from overloading your server.
@@ -38,29 +15,29 @@ sleep 10;                     #This puts a delay on how fast the script can run,
   DAYS=$DAYS+1
   DTR=$((7-$DAYS))
   DTU=$((14-$DAYS))
-  log "AutoUpdate/Restart check ran successfully at $TIME0" "Days until next restart '$DTR'" "Days until next update '$DTU'"
-  discord_log "7"
+  bash log "AutoUpdate/Restart check ran successfully at $TIME0" "Days until next restart '$DTR'" "Days until next update '$DTU'"
+  bash discord_log "7"
   fi
   if [[ "$DAYS" = "7" ]]; then
-  log "WARNING, '$serverip' will restart in 10 seconds... A second message should send when the server has successfully rebooted..."
-  discord_log "7"
+  bash log "WARNING, '$serverip' will restart in 10 seconds... A second message should send when the server has successfully rebooted..."
+  bash discord_log "7"
   sudo reboot
   fi
   if [[ "$DAYS" = "14" ]]; then
   DAYS=0
   if ! { sudo apt-get update 2>&1 || echo E: update failed; } | grep -q '^[WE]:'; then #run update and check for errors
-		log "SERVER UPDATE SUCCESSFUL"
-		discord_log "7"
+		bash log "SERVER UPDATE SUCCESSFUL"
+		bash discord_log "7"
 		else
-		log "ERROR, UPDATE ON '$serverip' FAILED."
+		bash log "ERROR, UPDATE ON '$serverip' FAILED."
 		fi
 		
 	  if ! { sudo apt-get upgrade 2>&1 || echo E: upgrade failed; } | grep -q '^[WE]:'; then #run upgrade and check for errors
-		log "SERVER UPGRADE SUCCESSFUL"
-		discord_log "7"
+		bash log "SERVER UPGRADE SUCCESSFUL"
+		bash discord_log "7"
 		else
-		log "ERROR, UPGRADE ON '$serverip' FAILED."
-		discord_log "7"
+		bash log "ERROR, UPGRADE ON '$serverip' FAILED."
+		bash discord_log "7"
 		fi
   fi
 done
