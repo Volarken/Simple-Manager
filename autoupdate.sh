9b7e1ca1bfb851acb6e42534f22f97457f4de16b
Root <24883953+Volarken@users.noreply.github.com>
2021-08-06 11:37:37 -0700

    fdg

--- autoupdate.sh
+++ autoupdate.sh
@@ -7,7 +7,7 @@ declare -i DAYS
 ##
 LogInput="AutoUpdate/Restart script is now online."
 sudo bash log.sh "$LogInput"
-sudo bash discord_log.sh "hookSend" "$LogInput" "whGREEN"
+sudo bash discord_log.sh "hookSend" "$LogInput" "$whGREEN"
 while [[ "0" = "0" ]]; do     #This is my lazy way of making sure that the script is constantly looping.
 TIME1=$(date +%H:%M)          #This stores the current time inside of a variable called TIME1
 sleep 10;                     #This puts a delay on how fast the script can run, this stops the script from overloading your server.
@@ -21,7 +21,7 @@ sleep 10;                     #This puts a delay on how fast the script can run,
   if [[ "$DAYS" = "7" ]]; then
   LogInput="WARNING, '$serverip' will restart in 10 seconds... A second message should send when the server has successfully rebooted..."
   sudo bash log.sh "$LogInput"
-  sudo bash discord_log.sh "hookSend" "$LogInput" "whRED"
+  sudo bash discord_log.sh "hookSend" "$LogInput" "$whRED"
   sudo reboot
   fi
   if [[ "$DAYS" = "14" ]]; then
@@ -29,21 +29,21 @@ sleep 10;                     #This puts a delay on how fast the script can run,
   if ! { sudo apt-get update 2>&1 || echo E: update failed; } | grep -q '^[WE]:'; then #run update and check for errors
 		LogInput="SERVER UPDATE SUCCESSFUL. $TIME0"
 		  sudo bash log.sh "$LogInput"
-		  sudo bash discord_log.sh "hookSend" "$LogInput" "whGREEN"
+		  sudo bash discord_log.sh "hookSend" "$LogInput" "$whGREEN"
 		else
 		LogInput="ERROR, UPDATE ON '$serverip' FAILED."
 		 sudo bash log.sh "$LogInput"
-		 sudo bash discord_log.sh "hookSend" "$LogInput" "whRED"
+		 sudo bash discord_log.sh "hookSend" "$LogInput" "$whRED"
 		fi
 		
 	  if ! { sudo apt-get upgrade 2>&1 || echo E: upgrade failed; } | grep -q '^[WE]:'; then #run upgrade and check for errors
 		LogInput="SERVER UPGRADE SUCCESSFUL"
 		sudo bash log.sh "$LogInput"
-		sudo bash discord_log.sh "hookSend" "$LogInput" "whGREEN"
+		sudo bash discord_log.sh "hookSend" "$LogInput" "$whGREEN"
 		else
 		LogInput="ERROR, UPGRADE ON '$serverip' FAILED. $TIME0"
 		sudo bash log.sh "$LogInput"
-		sudo bash discord_log.sh "hookSend" "$LogInput" "whGREEN"
+		sudo bash discord_log.sh "hookSend" "$LogInput" "$whGREEN"
 		fi
   fi
 done
