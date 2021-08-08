1da50b787ca65694665c74ff959f92f859f672de
Volarken <24883953+Volarken@users.noreply.github.com>
2021-08-08 11:24:16 -0500

    Update autoupdate.sh

--- autoupdate.sh
+++ autoupdate.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 #Global Variables##
-source $HOME/SimpleManager/global.var
+source global.var
+
 
 ##Child Script Variables##
 declare -i DAYS
 ##
 LogInput="AutoUpdate/Restart script is now online."
-sudo bash log.sh "$LogInput"
+sudo bash log "$LogInput"
 python3 send.py "$whGREEN" "$LogInput" "$TIME0"
 while [[ "0" = "0" ]]; do     #This is my lazy way of making sure that the script is constantly looping.
 TIME1=$(date +%H:%M)          #This stores the current time inside of a variable called TIME1
@@ -16,12 +17,12 @@ sleep 10;                     #This puts a delay on how fast the script can run,
   DTR=$((7-$DAYS))
   DTU=$((14-$DAYS))
   LogInput="AutoUpdate/Restart check ran successfully at $TIME0" "Days until next restart '$DTR'" "Days until next update '$DTU'"
-  sudo bash log.sh "$LogInput"
+  sudo bash log "$LogInput"
   python3 send.py "$whGREEN" "$LogInput" "$TIME0"
   fi
   if [[ "$DAYS" = "7" ]]; then
   LogInput="WARNING, '$serverip' will restart in 10 seconds... A second message should send when the server has successfully rebooted..."
-  sudo bash log.sh "$LogInput"
+  sudo bash log "$LogInput"
   python3 send.py "$whRED" "$LogInput" "$TIME0"
   sleep 10;
   sudo reboot
@@ -30,21 +31,21 @@ sleep 10;                     #This puts a delay on how fast the script can run,
   DAYS=0
   if ! { sudo apt-get update 2>&1 || echo E: update failed; } | grep -q '^[WE]:'; then #run update and check for errors
 		LogInput="SERVER UPDATE SUCCESSFUL."
-		  sudo bash log.sh "$LogInput"
+		  sudo bash log "$LogInput"
 		  python3 send.py "$whGREEN" "$LogInput" "$TIME0"
 		else
-		LogInput="ERROR, UPDATE ON '$serverip' FAILED."
-		 sudo bash log.sh "$LogInput"
+		LogInput="ERROR, UPDATE ON $serverip FAILED."
+		 sudo bash log "$LogInput"
 		 python3 send.py "$whRED" "$LogInput" "$TIME0"
 		fi
 		
 	  if ! { sudo apt-get upgrade 2>&1 || echo E: upgrade failed; } | grep -q '^[WE]:'; then #run upgrade and check for errors
 		LogInput="SERVER UPGRADE SUCCESSFUL"
-		sudo bash log.sh "$LogInput"
+		sudo bash log "$LogInput"
 		python3 send.py "$whGREEN" "$LogInput" "$TIME0"
 		else
-		LogInput="ERROR, UPGRADE ON '$serverip' FAILED. $TIME0"
-		sudo bash log.sh "$LogInput"
+		LogInput="ERROR, UPGRADE ON $serverip FAILED. $TIME0"
+		sudo bash log "$LogInput"
 		python3 send.py "$whGREEN" "$LogInput" "$TIME0"
 		fi
   fi
