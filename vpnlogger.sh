470685d95f99175ebc3ce18d6a85b30e192c7fd2
Root <24883953+Volarken@users.noreply.github.com>
2021-08-13 09:16:53 -0700

    Update Version 1.2.0

--- vpnlogger.sh
+++ vpnlogger.sh
@@ -4,25 +4,28 @@ cd /etc/SimpleManager/
 source global.var
 
 
-#cat /var/log/syslog | grep "MULTI_sva: pool returned" > x.txt
 
 ##Child Script Variables##
 ##
 LogInput="VPN Logger script is now online."
 sudo bash log "$LogInput"
 python3 send.py "$whGREEN" "$LogInput" "$TIME0"
+VPNLOG1="$(wc -l < /var/log/syslog)"
 while [[ "0" = "0" ]]; do     #This is my lazy way of making sure that the script is constantly looping.
-sleep 10;                     #This puts a delay on how fast the script can run, this stops the script from overloading your server.  
-  sudo cat /var/log/syslog | grep "MULTI_sva" | grep "@raymore" >> cons.txt
-  sudo cat /var/log/syslog | grep "MULTI_sva" | grep "@raymore" >> connection-history.log
+sleep 10;                     #This puts a delay on how fast the script can run, this stops the script from overloading your server.
+VPNLOG2="$(wc -l < /var/log/syslog)"
+  if [[ "$VPNLOG2" > "$VPNLOG1" ]]; then     
+  NEWLINES=$((VPNLOG2-VPNLOG1))
+  tail -$NEWLINES /var/log/syslog | grep 'MULTI_sva' >> cons.txt
   fi
   if test -f /etc/SimpleManager/cons.txt ; then 
   if grep -qwF "MULTI_sva" /etc/SimpleManager/cons.txt ; then
   NEWCONNECTION=$(cat /etc/SimpleManager/cons.txt)
-  LogInput="New VPN connection on server! -Details- $NEWCONNECTION "
+  LogInput="New connection on server! -Details- $NEWCONNECTION "
   sudo bash log "$LogInput"
   python3 send.py "$whBLUE" "$LogInput" "$TIME0"
   fi
   rm /etc/SimpleManager/cons.txt
   fi
+  VPNLOG1="$(wc -l < /var/log/syslog)"
 done
