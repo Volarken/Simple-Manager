ccaed8a2668c8b1aac7d6b77df714442d6462314
Root <24883953+Volarken@users.noreply.github.com>
2021-08-13 11:52:32 -0700

    Update send.py

--- send.py
+++ send.py
@@ -22,6 +22,9 @@ if __name__ == "__main__":
             "color" : "{}".format(a),
             "description" : "Activity: {}\n\n Time of Log: {}".format(b, c),
             "title" : "Event Logged!",
+            "thumbnail" : {
+            "url" : "https://www.startpage.com/av/proxy-image?piurl=https%3A%2F%2Fwww.raymore.com%2Fhome%2Fshowpublishedimage%2F882%2F636398689391530000&sp=1628873515T93c00d8dcaefb00cca2c367aa122d460ed84156af23466dd92c57a0dd1cd8e43",
+            }
 			"footer" : {
 			"text" : "Server IP: {}".format(ip)
 			}
